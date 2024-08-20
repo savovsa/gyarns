903dcc5978591c951f869a500d8e18c19f6f384b
Sasho Savov <savovs@gmail.com>
2024-08-20 08:46:56 +0300

    add base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,8 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+let baseUrl = 'http://localhost:5173'
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -25,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: baseUrl,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm run dev',
-    url: 'http://localhost:5173',
+    url: baseUrl,
     reuseExistingServer: !process.env.CI,
   },
 });
