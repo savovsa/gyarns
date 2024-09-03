14038f2805c097bda06494c4a2e3d119859d238a
Vlady Veselinov <vladkodmc@gmail.com>
2024-09-03 22:13:05 +0100

    new product card

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,26 +1,22 @@
+import { Link } from '@remix-run/react'
 import { AddToCartButton } from './AddToCartButton'
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from './ui/card'
 
 export function ProductCard() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Merino wool blend</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <img
-          alt="Yarn product image"
-          className="w-full h-60 object-cover rounded-lg"
-          src="https://g-yarns.com/image/cache/catalog/Merino/50.50.1-600x500.jpg"
-        />
-        <p className="text-sm text-gray-500 dark:text-gray-400 mt-2">
-          100g / 3.5oz
-        </p>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <span className="text-lg font-bold">$12.99</span>
-        <AddToCartButton />
-      </CardFooter>
-    </Card>
+    <Link
+      to="#"
+      className="flex flex-col gap-2 hover:no-underline hover:opacity-30 transition-opacity duration-200"
+    >
+      <img
+        alt="Yarn product image"
+        className="w-full h-60 object-cover rounded-lg"
+        src="https://g-yarns.com/image/cache/catalog/Merino/50.50.1-600x500.jpg"
+      />
+      <div className="flex flex-col gap-0.5">
+        <span>Merino wool blend</span>
+        <span className="text-sm font-bold">$12.99</span>
+      </div>
+    </Link>
   )
 }
