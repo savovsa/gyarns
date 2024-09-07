a654a93ba749cd387c328bf21c974f6273b3cf06
Vlady Veselinov <vladkodmc@gmail.com>
2024-09-07 15:27:49 +0100

    isAdminRoute tests

--- app/lib/useIsAdminRoute.tsx
+++ app/lib/useIsAdminRoute.tsx
@@ -2,6 +2,10 @@ import { useLocation } from '@remix-run/react'
 
 export function useIsAdminRoute() {
   const location = useLocation()
-  const fragments = location.pathname.split('/')
+  return isAdminRoute(location.pathname)
+}
+
+export function isAdminRoute(pathname: string) {
+  const fragments = pathname.split('/')
   return fragments[1] === 'a'
 }
